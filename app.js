68ec8d0b1f8312c79b459319993a5b289942251a
William Madden <madden.william.d@gmail.com>
2016-11-08 22:56:04 -0800

    ignoring election anxiety, added yes/nexample instance , entiely commented out, to app.js

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 'use strict';
 
-var nameQuestionAnsw = prompt('What is your namne?');
 
-console.log('First questions answered with ' + nameQuestionAnsw);
-alert('Hey ' + nameQuestionAnsw + '! You Win at being awesome!');
-var whereYouFrom = prompt('Where are you from ' + nameQuestionAnsw + '? ');
+
+var nameQuestionAnsw = prompt('What is your namne?');
+console.log('First questions answered with: ' + nameQuestionAnsw + ', and stored in variable \'nameQuestionAnsw\'');
+alert('Hey ' + nameQuestionAnsw + '! You Win at being awesome!\n And now its time for some QUESTIONS!!!!!');
+alert('The left says \"yes\" and the right says \"no\" \nI\'m in between and the more I learn Well the less that I know.\n --Dennis DeYoung (Styx). Borrowed Time');
+
+var likeArt = prompt('Do you like art?');
+
+/*
+prototype of yes no check
+if(var .toLowerCase() === 'yes' || var  .toLowerCase() === 'y'){
+  console.log('The User answered \"yes\"');
+}else if( var .toLowerCase() === 'no' || var .toLowerCase === 'n'){
+  console.log('The User answered \"no\"');
+}else{
+    console.log('The User did not enter a valid \"yes\" or \"no\" answer');
+}
+*/
+
+prompt('Where are you from ' + nameQuestionAnsw + '? ');
 console.log(nameQuestionAnsw + ' is from ' + whereYouFrom);
 alert('Howdy ' + nameQuestionAnsw + ' from ' + whereYouFrom);
 var ageAnsw = prompt('hey, ' + nameQuestionAnsw + ' from ' + whereYouFrom + ', how old are you?');
