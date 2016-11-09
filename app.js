34938d980aaf539ead28e9cb3093508b1c0c4bcb
William Madden <madden.william.d@gmail.com>
2016-11-08 19:49:18 -0800

    Breaking for dinner (BLT BTW), pausing miupdate, changes made in style.csspy edited on index.html, andsome starting js added to app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var nameQuestionAnsw = prompt('What is your namne?');
+
+console.log('First questions answered with ' + nameQuestionAnsw);
+alert('Hey ' + nameQuestionAnsw + '! You Win at being awesome!');
+var whereYouFrom = prompt('Where are you from ' + nameQuestionAnsw + '? ');
+console.log(nameQuestionAnsw + ' is from ' + whereYouFrom);
+alert('Howdy ' + nameQuestionAnsw + ' from ' + whereYouFrom);
+var ageAnsw = prompt('hey, ' + nameQuestionAnsw + ' from ' + whereYouFrom + ', how old are you?');
+console.log(nameQuestionAnsw + ' from ' + whereYouFrom + ' is ' + ageAnsw + ' years old.');
+alert('Wow! ' + nameQuestionAnsw + ' from ' + whereYouFrom + ageAnsw + 'is really old!');
+var whereToNow = prompt('So, ' + nameQuestionAnsw + ' from ' + whereYouFrom + ', after ' + ageAnsw + ' years, where are you headed to now?');
+console.log ( nameQuestionAnsw + ' from ' + whereYouFrom + ' is ' + ageAnsw + ' years young, and heading to ' + whereToNow);
+alert( nameQuestionAnsw + ' from ' + whereYouFrom + ' is ' + ageAnsw + ' years young, and heading to ' + whereToNow);
