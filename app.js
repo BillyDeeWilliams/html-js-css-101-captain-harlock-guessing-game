ebe1a21aa8d848f7a7b162f1a5750d3512a0e0f5
William Madden <madden.william.d@gmail.com>
2016-11-09 20:37:17 -0800

    Modified js in app.js, loo for first 5 yes or no questions almost done, beginning to develope guess a number game

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ var ynanswers = [];
 var usergueses = [];
 var ynIsCorrect = [];
 
-var randmnum = '';
-var numquess = '';
+var randmNum = '';
+var guessRandm = '';
 
-var numguesses = '';
-var numcorrect = '';
-var numwrong = '';
+var numOfGuesses = '';
+var numCorrect = '';
+var numWrong = '';
 var qsreamin = '';
 var score = '';
 
-var userguess = '';
-var possibleanswers = [];
+var guessWHere = '';
+var possibleAnswers = [];
 
 //start with 5 yes or no QUESTIONS
 //populate ynquestions with yes or no QUESTIONS
@@ -43,3 +43,40 @@ ynanswers.push(' 2nd answer (yes or no)');
 ynanswers.push(' 3rd answer (yes or no)');
 ynanswers.push(' 4th answer (yes or no)');
 ynanswers.push(' 5th answer (yes or no)');
+
+console.log('yes no question array: ' + ynquestions + '\n answer key array: ' + ynanswers );
+// QUESTION: i thought that console.log did not require a '+' opperand to concatinate the console output //
+
+for ( var i = 0 ; i < ynquestions.length ; i++ ){
+  userguesses.push(prompt(ynquestions[i]));
+  console.log('user guesses for answer question ' + i + ': ' + userguesses[i] );
+  if{yes
+    if yes === ynanswers[i]{
+
+      you got question i correct!
+      correct answere ++1    }
+      else{
+        you got it wrong
+        wrong answer ++1
+      }
+   }
+  if else  no or n{
+    if() no === ynanswers[i]{
+      you got it right!
+      correct answer ++1
+    }else{
+      you got it wrong
+      wrong answer ++1
+    }
+  }else{
+    when your input is invalid you get the question wrong.
+    wrong answer ++1
+  }
+}/* ask for as many answers as there are questions and store the answer in userguesses array,
+ console out userguesses array for debugging, one at a time in the loop and inits entirety right after */
+console.log('userguesses: ' + userguesses);
+
+//guess a number game!
+randmnum = x
+you get 3 tries
+is guess 
