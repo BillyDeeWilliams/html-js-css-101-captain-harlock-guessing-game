f503218151babe74fe7f7c5039ebd7cbfa148309
William Madden <madden.william.d@gmail.com>
2016-11-09 19:51:08 -0800

    beginngint to configure css, html structure continues to be refine yes no question and answer key is populated in app.js

--- app.js
+++ app.js
@@ -27,3 +27,19 @@ var score = '';
 
 var userguess = '';
 var possibleanswers = [];
+
+//start with 5 yes or no QUESTIONS
+//populate ynquestions with yes or no QUESTIONS
+
+ynquestions.push(' yes or no question 1');
+ynquestions.push(' yes or no question 2');
+ynquestions.push(' yes or no question 3');
+ynquestions.push(' yes or no question 4');
+ynquestions.push(' yes or no question 5');
+
+//populate answer key
+ynanswers.push(' first answer (yes or no)');
+ynanswers.push(' 2nd answer (yes or no)');
+ynanswers.push(' 3rd answer (yes or no)');
+ynanswers.push(' 4th answer (yes or no)');
+ynanswers.push(' 5th answer (yes or no)');
