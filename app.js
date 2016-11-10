10486cdfad7d76008f811c151113db780e1d09b0
William Madden <madden.william.d@gmail.com>
2016-11-10 13:27:24 -0800

    having done updates durring luch on THursday, functionality should be working, im colling it done fore noe and merging and push/pull before inclass lab today

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var numWrong = '0';
 
 //start with 5 yes or no QUESTIONS
 //populate ynquestions with yes or no QUESTIONS
-
+// this is not a loop because i do not want the user to ever have conrtoll of changing the questions or the answer key
 ynquestions.push(' Was Captain Harlock born on Mars?');
 ynquestions.push(' Has Captain Harlock ever been arrested?');
 ynquestions.push(' Do any other criminals have a longer criminal record  than Harlock on Venus?');
@@ -48,9 +48,9 @@ console.log('yes no question array: ' + ynquestions + '\n answer key array: ' + 
 // QUESTION: i thought that console.log did not require a '+' opperand to concatinate the console output //
 
 for ( var i = 0 ; i < ynquestions.length ; i++ ){ //do this for as many times as there are questions
-  userguesses.push(prompt(ynquestions[i]).toLowerCase()); // toLowerCase redundencies intentional
+  userguesses.push(prompt(ynquestions[i]).toLowerCase()); // toLowerCase to controll comparison
   console.log('user guesses for answer question ' + i + ': ' + userguesses[i] );
-  if (userguesses[i].toLowerCase() === 'yes' || userguesses[i].toLowerCase() === 'y' ){
+  if (userguesses[i] === 'yes' || userguesses[i] === 'y' ){
     if (userguesses[i] === ynanswers[i]){
       console.log('guess: ' + userguesses[i] + 'correct answer: ' + ynanswers[i]);
       alert(' congrats! you got ' + ynquestions[i] + ' correct!');
@@ -62,7 +62,7 @@ for ( var i = 0 ; i < ynquestions.length ; i++ ){ //do this for as many times as
       numWrong = numWrong + 1;
     }
   }
-  else if (userguesses[i].toLowerCase() === 'no' || userguesses[i].toLowerCase() === 'n' ){
+  else if (userguesses[i] === 'no' || userguesses[i] === 'n' ){
     if (userguesses[i] === ynanswers[i]){
       console.log('guess: ' + userguesses[i] + 'correct answer: ' + ynanswers[i]);
       alert(' congrats! you got ' + ynquestions[i] + ' correct!');
@@ -87,18 +87,19 @@ alert(' you got ' + numCorrect + ' questions correct, and ' + numWrong + ' quest
 
 
 //guess a number game!
-randmNum = Math.floor(Math.random() * 10);
+randmNum = Math.floor(Math.random() * 10); //generates a random number between 0-9
 console.log('randmNum is ' + randmNum );
 alert('welcome to the Guess a Number Game! \n Im Thinking of a number between 0-10... \nYou\'ll get three attempts to guess.\n click OK when ready.') ;
 for ( var dtt = 0 ; dtt < 3; dtt++){ //do three times dtt //
   guessRandm = parseInt(prompt('What is your guess?'));
   if(guessRandm < randmNum){
     console.log('guessRandm is: ' + guessRandm + ' and randmNum is: ' + randmNum );
-    alert('Sorry that\'s too low \n you have ' + (3 - dtt + 1) + ' guesses left');
+    alert('Sorry that\'s too low \n you have ' + (3 - (dtt + 1)) + ' guesses left');
   }else if(guessRandm > randmNum){
-    alert('Sorry, that\'s too high \n you have ' + (3 - dtt + 1) + ' guesses left');
+    alert('Sorry, that\'s too high \n you have ' + (3 - (dtt + 1)) + ' guesses left');
   }else if (guessRandm === randmNum){
     alert('You WIN! \n' + guessRandm + ' is correct!');
+    break;
   }
   else {
     console.log('Unexpected Error. \n randmNum: ' + randmNum + '\n guessRandm: ' + guessRandm);
@@ -106,20 +107,28 @@ for ( var dtt = 0 ; dtt < 3; dtt++){ //do three times dtt //
 }
 
 // question seven//
+var isCorrect = false;
 var possibleAnswers = [ 'mars', 'europa', 'titan', 'io' , 'iss' ];
 alert('There are FIVE celestial bodies in our solar system where the bountry for Harlock is so high \n in order to avoid having to payout, Harlock has his own official Sol Law Enforcement standing Shoot-On-Sight and Shoot-To-Kill order.\n This includes moons, planets, dwarf planets, asteroids, and artificial satelites, can you guess one of them? \n You get 5 guesses, click OK to start');
 
-for ( var f = 0 ; f < 5 ; j++ ){
-  //you get 5 tries
+var f = 0;  //you get 6 tries
+do{
+  f++;
   var guessWHere = prompt('What is your guess? \n ex: Earth, Luna, Jupiter').toLowerCase();
   for (var j = 0; j < possibleAnswers.length ; j++){
-    if ( possibleAnswers[j] === guessWHere){
-      alert('THATS RIGHT! \n You Win!');
-      f = 6; // this should terminate the highest for loop
+    if ( guessWHere === possibleAnswers[j] ){
+      alert('THATS RIGHT! \n You Win!\n It took you' + f + 'tries');
+      isCorrect = true;
+      break;
+    } else{
+      console.log('position in array of answers: ' + j );
     }
-    else{
-      console.log('j: ' + j + ' possibleAnswers[j]: ' + possibleAnswers[j] );
-      alert('Sorry, try again');
+    if (isCorrect === false){
+      alert('Sorry, try again \n you have ' + (6 - f) + ' tries remining');
+    }
+    if( f === 6){
+      isCorrect = true;
     }
   }
 }
+while ( isCorrect === flase);
